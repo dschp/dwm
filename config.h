c58aa7df17a6e4509903d79ec396115707bb6e0f
theon <me@dschp.theoriun.com>
2024-10-05 21:53:21 +0700

    Rearrange color scheme

--- config.h
+++ config.h
@@ -9,23 +9,27 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "sans-serif:size=7" };
 static const char dmenufont[]       = "sans-serif:size=11";
-static const char col_gray1[]       = "#191919";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#dddddd";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#007799";
-static const char col_cyan2[]       = "#00bbff";
-static const char col_black[]       = "#000000";
-static const char col_white[]       = "#ffffff";
+static const char col_fg1[]         = "#dddddd";
+static const char col_fg2[]         = "#eeeeee";
+static const char col_bg1[]         = "#000000";
+static const char col_bg2[]         = "#191919";
+static const char col_bdr1[]        = "#2652ac";
+static const char col_bdr2[]        = "#242424";
+static const char col_cyan1[]       = "#00bbff";
+static const char col_cyan2[]       = "#0077cc";
+static const char col_yellow[]      = "#ffff00";
+static const char col_green[]       = "#afff00";
+static const char col_red[]         = "#ff0087";
+static const char col_aqua[]        = "#00dddd";
 
 static const char *colors[][3]      = {
-  /*                 fg         bg          border   */
-  [SchemeNorm]   = { col_gray3, col_gray1,  col_gray2 },
-  [SchemeSel]    = { col_cyan2, col_black,  col_cyan  },
-  [SchemeWarn]   = { "#ffff00", col_gray1,  col_gray2 },
-  [SchemeUrgent] = { "#afff00", col_gray1,  col_gray2 },
-  { "#ff0087", col_gray1,  col_gray2 },
-  { "#00dddd", col_gray1,  col_gray2 },
+  /*                 fg          bg        border   */
+  [SchemeNorm]   = { col_fg1,    col_bg2,  col_bdr2 },
+  [SchemeSel]    = { col_cyan1,  col_bg1,  col_bdr1 },
+  [SchemeWarn]   = { col_yellow, col_bg2,  col_bdr2 },
+  [SchemeUrgent] = { col_green,  col_bg2,  col_bdr2 },
+                   { col_red,    col_bg2,  col_bdr2 },
+                   { col_aqua,   col_bg2,  col_bdr2 },
 };
 
 /* tagging */
@@ -61,10 +65,10 @@ static const Layout layouts[] = {
 /* key definitions */
 #define MODKEY Mod4Mask
 #define TAGKEYS(KEY,TAG)						\
-  { MODKEY,                       KEY,      view,           {.ui = 1UL << TAG} }, \
-  { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1UL << TAG} }, \
-  { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1UL << TAG} }, \
-  { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1UL << TAG} },
+  { MODKEY,                       KEY,      view,           {.ui = 1ULL << TAG} }, \
+  { MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1ULL << TAG} }, \
+  { MODKEY|ShiftMask,             KEY,      tag,            {.ui = 1ULL << TAG} }, \
+  { MODKEY|ControlMask|ShiftMask, KEY,      toggletag,      {.ui = 1ULL << TAG} },
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
@@ -72,10 +76,10 @@ static const char *dmenucmd[] = {
   "dmenu_run",
   "-m",   dmenumon,
   "-fn",  dmenufont,
-  "-nb",  col_gray1,
-  "-nf",  col_gray3,
-  "-sb",  col_cyan,
-  "-sf",  col_gray4,
+  "-nb",  col_bg2,
+  "-nf",  col_fg1,
+  "-sb",  col_cyan2,
+  "-sf",  col_fg2,
   NULL };
 static const char *termcmd[]  = { "st", NULL };
 
@@ -165,4 +169,3 @@ static Button buttons[] = {
   { ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
   { ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
